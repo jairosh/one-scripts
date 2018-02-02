28b82760759cc638576dea65ae15efbdb4fedce9
Jairo Sánchez <jairojsh@gmail.com>
2018-02-02 16:39:40 -0600

    Added progress report

--- process_bulk_reports.py
+++ process_bulk_reports.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: jairo
 # @Date:   2018-02-02 15:40:47
-# @Last Modified by:   jairo
-# @Last Modified time: 2018-02-02 15:55:58
+# @Last Modified by:   Jairo Sánchez
+# @Last Modified time: 2018-02-02 16:35:58
 
 import pandas as pd
 import os
@@ -18,16 +18,20 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dir', type=str, required=True,
                         help='Directory that contains the reports')
+    parser.add_argument('-o', '--output', type=str, required=False,
+                        help='Destination file for CSV output')
     args = parser.parse_args()
     if os.path.isdir(args.dir):
         collector = pd.DataFrame()
         pattern = re.compile(REGEX)
-        for file in os.listdir(args.dir):
+        reports = os.listdir(args.dir)
+        total = float(len(reports))
+        processed = 0.0
+        for file in reports:
             reportfile = os.path.join(args.dir, file)
             df = pd.read_table(reportfile, sep=':')
             df1 = df.T
             vars = pattern.match(file).groups()
-            print(vars)
             df1['seed'] = vars[0]
             df1['nodes'] = vars[1]
             df1['alpha'] = vars[2]
@@ -35,8 +39,15 @@ def main():
             df1['delta'] = vars[4]
             df1['gamma'] = vars[5]
             collector = collector.append(df1, ignore_index=True)
+            processed += 1.0
+            print('\r{0:04}% '.format(processed * 100.0 / total), end=' ')
 
-        print(collector)
+        if args.output:
+            collector.to_csv(args.output, index=False)
+        print('Done')
+    else:
+        print('ERROR: Invalid directory')
+        exit(2)
 
 
 if __name__ == '__main__':
